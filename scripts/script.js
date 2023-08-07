f24d3072816f9a5835a62834c704062e901fd7b2
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-08-07 12:26:22 +0100

    add variables for all buttons

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const clearBtn = document.querySelector('[data-clear]');
+const deleteBtn = document.querySelector('[data-delete]');
+const equalsBtn = document.querySelector('[data-equals]');
+const numberBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operator]');
+
+const previousText = document.querySelector('[data-previous]');
+const currentText = document.querySelector('[data-current]');
