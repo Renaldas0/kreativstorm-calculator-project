c84318965365c122d2c4b89470ff5c1d154364cd
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-08-07 15:39:21 +0100

    make a start on calculator functionality

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,36 @@ const operatorBtns = document.querySelectorAll('[data-operator]');
 
 const previousText = document.querySelector('[data-previous]');
 const currentText = document.querySelector('[data-current]');
+
+function displayValue(value) {
+    currentText.textContent = value;
+};
+
+numberBtns.forEach(button => {
+    button.addEventListener("click", function () {
+        const value = button.value;
+        displayValue(value);
+    });
+});
+
+function add(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+};
+function subtract(num1, num2) {
+    let sum = num1 - num2;
+    return sum;
+};
+function multiply(num1, num2) {
+    let sum = num1 * num2;
+    return sum;
+};
+function divide(num1, num2) {
+    let sum = num1 / num2;
+    return sum;
+};
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
